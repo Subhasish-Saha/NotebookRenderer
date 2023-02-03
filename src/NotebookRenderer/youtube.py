fb97198448bae8654df95293bccf28900db6ce66
Subhasish <subhasishsaha007@gmail.com>
2023-02-03 12:18:11 +0530

    formatting done using black

--- src/NotebookRenderer/youtube.py
+++ src/NotebookRenderer/youtube.py
@@ -6,7 +6,6 @@ from py_youtube import Data
 
 
 @ensure_annotations
-
 def get_time_info(URL: str) -> int:
     def _verify_vid_id_len(vid_id, __expected_len=11):
         len_of_vid_id = len(vid_id)
